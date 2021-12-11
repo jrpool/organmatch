97a2a16eba2923f163361c7e7a344a99e8799aec
Jonathan Pool <pool@stulta.com>
2021-12-11 08:13:31 -0500

    Implemented pruneSessions

---
+++ pruneSessions.js
@@ -0,0 +1,24 @@
+/*
+  pruneSessions
+  Deletes expired sessions of OrganMatch.
+  Arguments:
+  0: expiration time in minutes.
+*/
+// IMPORTS
+const fs = require('fs');
+// FUNCTIONS
+// Deletes expired session files.
+const pruneSessions = () => {
+  const now = Date.now();
+  const maxBirthTime = now - 60000 * process.argv[2];
+  const fileNames = fs.readdirSync('on');
+  fileNames.forEach(fileName => {
+    const path = `on/${fileName}`;
+    const birthTime = fs.statSync(path).birthtimeMs;
+    if(birthTime < maxBirthTime) {
+      fs.rmSync(path);
+    }
+  });
+}
+// OPERATION
+pruneSessions();
