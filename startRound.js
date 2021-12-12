8c68e55e6380633f83e0cac795fcaca0a06c814f
Jonathan Pool <pool@stulta.com>
2021-12-12 13:13:07 -0500

    Created startRound

---
+++ startRound.js
@@ -0,0 +1,49 @@
+/*
+  startRound
+  Starts a round of OrganMatch.
+  Arguments:
+  0: session code (e.g., 45678).
+*/
+// IMPORTS
+const fs = require('fs');
+// CONSTANTS
+const sessionCode = process.argv[2];
+// FUNCTIONS
+// Starts a round.
+const startRound = () => {
+  try {
+    const sessionJSON = fs.readFileSync(`on/${sessionCode}.json`, 'utf8');
+    const sessionData = JSON.parse(sessionJSON);
+    const {startTime, endTime, piles, rounds} = sessionData;
+    if (endTime) {
+      return 'sessionAlreadyEnded';
+    }
+    else if (startTime) {
+      if (piles.latent.organ.length) {
+        if (piles.current.organ.length) {
+          piles.expired.organ.push(piles.current.organ.shift());
+        }
+        piles.current.organ.push(piles.latent.organ.shift());
+        const starter = rounds.length ? rounds[rounds.length - 1].winner : 0;
+        rounds.push({
+          startTime: Date.now(),
+          starter,
+          currentOrgan: piles.current.organ[0],
+          turns: []
+        });
+        fs.writeFileSync(`on/${sessionCode}.json`, `${JSON.stringify(sessionData, null, 2)}\n`);
+      }
+      else {
+        return `organCardsExhausted`;
+      }
+    }
+    else {
+      return 'sessionNotYetStarted';
+    }
+  }
+  catch (error) {
+    return error.message;
+  }
+};
+// OPERATION
+console.log(startRound());
