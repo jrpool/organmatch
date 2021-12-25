235f138e01fce961e8691881e5be37fcbd00ae30
Jonathan Pool <pool@stulta.com>
2021-12-25 11:47:47 -0500

    Added name to status page

--- playerStatus.js
+++ playerStatus.js
@@ -1,11 +1,13 @@
 // playerStatus
 const params = JSON.parse(document.getElementById('params').textContent);
-// Add the session code and the player list to the page.
+// Add the session code, player name, and player list to the page.
+const playerName = params.name;
 document.getElementById('sessionCode').textContent = params.sessionCode;
+document.getElementById('playerName').textContent = playerName;
 const playerOL = document.getElementById('playerList');
 playerOL.innerHTML = params.playerList;
 const newPlayerLI = document.createElement('li');
-newPlayerLI.innerText = params.name;
+newPlayerLI.innerText = playerName;
 playerOL.appendChild(newPlayerLI);
 // Add players to the list when they join.
 const playerLister = new EventSource('/playerJoined');
